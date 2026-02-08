378eda7e1cf32a81b115b9e108d3507684e7675b
Nerok <nerok1701@gmail.com>
2026-02-08 15:29:20 -0500

    Delete WantanDevs_RPG - copia/wantan-devs-rpg directory

--- WantanDevs_RPG - copia/wantan-devs-rpg/Escenas/Codigos/scene_trigger.gd
+++
@@ -1,8 +0,0 @@
-class_name SceneTrigger extends Area2D
-
-@export var connected_scene: String
-
-
-func _on_body_entered(body):
-	if body is Player:
-		scene_manager.change_scene(get_owner(),connected_scene)
